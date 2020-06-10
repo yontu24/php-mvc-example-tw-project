9e72bac48eefc495c5d8575a7dede36e3a4549fa
SebastianAp27 <61700621+SebastianAp27@users.noreply.github.com>
2020-06-10 22:33:27 +0300

    2xdropdown,2xchart

---
+++ Proiect/MCV/View/valdoileachart.js
@@ -0,0 +1,28 @@
+import {CAlDoileaChart} from "../Controller/caldoileachart.js";
+	
+function drawSecondChart(locatie,an) {
+	let obese = CAlDoileaChart(locatie,an);
+	console.log(obese);
+	var data = new google.visualization.DataTable();
+
+	data.addColumn('string', 'IDcategorie');
+	data.addColumn('number', 'Cazuri');
+
+	for(var i=0 ; i < obese.length ; i++)
+	{
+		data.addRows([[obese[i].label , obese[i].y]
+			]);
+	}
+	console.log(data);
+	var options = {
+	title: 'Obesity by age category in year '+an,
+	legend: { position: 'left' }
+	};
+
+	var chart = new google.visualization.PieChart(document.getElementById('curve_chart'));
+
+	chart.draw(data, options);
+}
+
+
+export {drawSecondChart};
