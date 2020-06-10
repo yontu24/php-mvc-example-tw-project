8de5f06711c89fce9118067d97d8f900c14e9a67
IvascuVlad <61700616+IvascuVlad@users.noreply.github.com>
2020-06-10 12:12:58 +0300

    Integrare functii Sebi

---
+++ Proiect/MCV/Controller/dropdownOptionSelected.js
@@ -0,0 +1,16 @@
+//let theDropdown = document.getElementById("locality-dropdown");
+import {functie1} from "./functie1.js";
+import {drawChart}from "../View/functie2.js";
+
+let theDropdown = document.getElementById("locality-dropdown");
+theDropdown.onclick = function () {
+    for (var i = 1; i < theDropdown.options.length; i++) {
+        if (theDropdown.options[i].selected == true) {
+			let a=functie1(theDropdown.options[i].text);
+			let b=drawChart(a,theDropdown.options[i].text);
+			b.render();
+        }
+    }
+ }
+
+
---
+++ Proiect/MCV/Controller/functie1.js
@@ -0,0 +1,53 @@
+function functie1(locatie){
+	const url = 'http://localhost/TW/Proiect_TW/Model/mgetbylocation.php';
+	const xhr = new XMLHttpRequest();
+	var obese = [];
+	var dataPoints2 = [];
+	xhr.onload = () => {
+		var anul=0;
+		var ob=0;
+		
+		if (xhr.status >= 200 && xhr.status < 300) {
+			const response = JSON.parse(xhr.responseText);
+			for (var i = 0; i < response.values.length; i++)
+			{
+			if (parseInt(response.values[i].an, 10) != anul) {
+				obese.push({
+					an: anul,
+					cazuri: ob
+				});
+				anul = (parseInt(response.values[i].an, 10));
+				ob = 0;
+			}
+
+			if (response.values[i].categorie.includes("Obese"))
+		
+				ob = ob + (parseInt(response.values[i].cazuri, 10));
+		
+			}
+			obese.shift();
+			for(var i=0;i<obese.length;i++)
+			{
+				dataPoints2.push({ y:obese[i].cazuri ,label :obese[i].an});
+			}
+			
+		};
+	}
+
+	const json = {
+		"locatie": locatie
+	};
+	console.log(json);
+	console.log(locatie);
+
+	xhr.open('POST', url);
+
+	xhr.setRequestHeader('Content-Type', 'application/json');
+
+	xhr.send(JSON.stringify(json));
+	
+	return dataPoints2;
+
+}
+
+export {functie1};
