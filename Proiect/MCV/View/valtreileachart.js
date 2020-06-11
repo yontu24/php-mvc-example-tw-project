8a18d60041e072742e1be2fb748e9463c6b7e61b
SebastianAp27 <61700621+SebastianAp27@users.noreply.github.com>
2020-06-11 16:06:56 +0300

    view cu export

--- Proiect/MCV/View/valtreileachart.js
+++ Proiect/MCV/View/valtreileachart.js
@@ -20,8 +20,42 @@ function drawThirdChart(locatie,an) {
 	};
 
 	var chart = new google.visualization.BarChart(document.getElementById('bar_chart'));
+	var btnSave = document.getElementById('savepng2');
 
+	google.visualization.events.addListener(chart, 'ready', function () {
+			btnSave.disabled = false;
+	  });
+
+	btnSave.addEventListener('click', function () {
+		
+		var imgUri=chart.getImageURI();
+		let link = document.createElement('a');
+		link.href = imgUri;
+		link.download = "chart.png";
+		link.click();
+		link = null;
+	  }, false);
+	  var btnSave1 = document.getElementById('savepdf2');
+     google.visualization.events.addListener(chart, 'ready', function () {
+	  btnSave1.disabled = false;
+	  });
+
+	  btnSave1.addEventListener('click', function () {
+    var doc = new jsPDF();
+    doc.addImage(chart.getImageURI(), 0, 0);
+    doc.save('chart.pdf');
+  }, false); 
+	  
 	chart.draw(data, options);
+	document.getElementById("savecsv").style.display = "none";
+	document.getElementById("savecsv1").style.display = "block";
+	document.getElementById("savecsv2").style.display = "block";
+	document.getElementById("savepdf").style.display = "none";
+	document.getElementById("savepdf1").style.display = "block";
+	document.getElementById("savepdf2").style.display = "block";
+	document.getElementById("savepng").style.display = "none";
+	document.getElementById("savepng1").style.display = "block";
+	document.getElementById("savepng2").style.display = "block";
 }
 
 
--- Proiect/MCV/View/vprimulchart.js
+++ Proiect/MCV/View/vprimulchart.js
@@ -1,5 +1,5 @@
 import {CPrimulChart} from "../Controller/cprimulchart.js";
-	
+
 function drawFirstChart(locatie) {
 	let obese = CPrimulChart(locatie);
 	var data = new google.visualization.DataTable();
@@ -19,9 +19,45 @@ function drawFirstChart(locatie) {
 	};
 
 	var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+	
+	var btnSave = document.getElementById('savepng');
 
+	google.visualization.events.addListener(chart, 'ready', function () {
+			btnSave.disabled = false;
+	  });
+
+	btnSave.addEventListener('click', function () {
+		
+		var imgUri=chart.getImageURI();
+		let link = document.createElement('a');
+		link.href = imgUri;
+		link.download = "chart.png";
+		link.click();
+		link = null;
+	  }, false);
+	
+	var btnSave1 = document.getElementById('savepdf');
+    google.visualization.events.addListener(chart, 'ready', function () {
+    btnSave1.disabled = false;
+  });
+
+  btnSave1.addEventListener('click', function () {
+    var doc = new jsPDF();
+    doc.addImage(chart.getImageURI(), 0, 0);
+    doc.save('chart.pdf');
+  }, false); 
+  
+	
+	
 	chart.draw(data, options);
+	document.getElementById("savecsv").style.display = "block";
+	document.getElementById("savecsv1").style.display = "none";
+	document.getElementById("savecsv2").style.display = "none";
+	document.getElementById("savepdf").style.display = "block";
+	document.getElementById("savepdf1").style.display = "none";
+	document.getElementById("savepdf2").style.display = "none";
+	document.getElementById("savepng").style.display = "block";
+	document.getElementById("savepng1").style.display = "none";
+	document.getElementById("savepng2").style.display = "none";
 }
-
-
-export {drawFirstChart};
+export{drawFirstChart};
